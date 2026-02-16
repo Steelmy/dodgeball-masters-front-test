b8d32baa88877cb2827e3158c95371068eaac5b6
Ory-Migeon Liam <ory-migeonliam@MacBook-Air-de-Liam.local>
2026-02-16 14:02:14 +0100

    Update base URL for GitHub Pages

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,6 @@ import { defineConfig } from "vite";
 
 export default defineConfig({
   // Base path pour GitHub Pages
-  // Correspond au nom de ton dépôt : /dodgeball-masters-front/
-  base: "/dodgeball-masters-front/",
+  // Correspond au nom de ton dépôt : /dodgeball-masters-front-test/
+  base: "/dodgeball-masters-front-test/",
 });
